03cfa47c0d07da83d6706331e6a8bcccaf382787
Zuleidy Briceno <zuleidyb@gmail.com>
2026-07-16 09:57:38 -0400

    Add Vercel Analytics to portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Skills from './components/Skills'
 import Education from './components/Education'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Education />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   )
 }
